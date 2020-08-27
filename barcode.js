cbc7a2bd293ffe26f940319f36ae68d70a0123c7
Nurul Huda <nurul.starit@gmail.com>
2020-08-27 15:54:08 +0600

    modification

--- barcode.js
+++ barcode.js
@@ -76,14 +76,7 @@ $(function() {
     // Once a barcode had been read successfully, stop quagga and 
     // close the modal after a second to let the user notice where 
     // the barcode had actually been found.
-    Quagga.onDetected(function(result) {    		
-        if (result.codeResult.code){
-            $('#scanner_input').val(result.codeResult.code);
-            console.log(result.codeResult.code)
-            Quagga.stop();	
-            setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);			
-        }
-    });
+    
     
     // Stop quagga in any case, when the modal is closed
     $('#livestream_scanner').on('hide.bs.modal', function(){
